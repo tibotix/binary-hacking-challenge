62d16425966da73d79a99c9c6cdceb1177d3a3e5
tibotix <tizian@seehaus.net>
2024-11-27 01:42:10 +0100

    ref: move ICU to separate header

--- cpue/include/cpu.h
+++ cpue/include/cpu.h
@@ -3,6 +3,7 @@
 #include <stack>
 
 #include "mmu.h"
+#include "icu.h"
 #include "address.h"
 #include "disassembler.h"
 #include "pic.h"
@@ -14,79 +15,6 @@ namespace CPUE {
 
 // TODO: when implementing protected instructions, see chapter 2.8: System Instruction Summary
 // TODO: when implementing some instructions, look at type checking (chapter 6.4 or page 3250)
-
-
-
-/**
- * Interrupt Controller Unit
- * This class encapsulates functionality related to interrupts and exceptions.
- * It acts as a centralized location to receive interrupts from:
- *  - the processor itself (processor-generated exceptions)
- *  - NMI pin
- *  - INTR pin
- */
-class ICU {
-public:
-    friend class CPU;
-    ICU() = default;
-    ICU(ICU const&) = delete;
-
-private:
-    static constexpr u8 MAX_PENDING_CAPACITY = 127;
-    _InterruptRaised raise_interrupt(Interrupt i, u8 priority) {
-        CPUE_ASSERT(1 <= priority <= 9, "priority must be between 1 and 9");
-        return _raise_interrupt(i, priority);
-    }
-    _InterruptRaised raise_interrupt(Interrupt i) {
-        TODO_NOFAIL("Implement proper interrupt priority setting");
-        u8 priority = [&]() -> u8 {
-            switch (i.type) {
-                case InterruptType::ABORT_EXCEPTION: return 1;
-                case InterruptType::TRAP_EXCEPTION: return 2;
-                case InterruptType::FAULT_EXCEPTION: return 7;
-                case InterruptType::NON_MASKABLE_INTERRUPT: return 5;
-                case InterruptType::MASKABLE_INTERRUPT: return 6;
-                default: break;
-            }
-            return 9;
-        }();
-        return raise_interrupt(i, priority);
-    }
-    /**
-     * Instruction integral interrupts are handled as an integral part of the current instruction,
-     * so they get a virtual highest priority.
-     * NOTE: If #PF and #GP occur during fetching of an instruction, they are not integral.
-     *       If #GP occur during decoding of an instruction (cause Instruction length > 15 bytes), they are not integral.
-     *       Otherwise they are integral.
-     * Integral Exceptions are: DE, BP, OF, BR, TS, NP, SS, GP, PF, AC, MF, XM, VE, CP,
-     */
-    _InterruptRaised raise_integral_interrupt(Interrupt i) { return _raise_interrupt(i, 0); }
-    _InterruptRaised nmi_raise_interrupt(Interrupt i) { fail("NMI pin is not implemented"); }
-
-    std::optional<Interrupt> pop_highest_priority_interrupt() {
-        if (m_pending_interrupts.empty())
-            return {};
-        auto prioritized_interrupt = m_pending_interrupts.top();
-        m_pending_interrupts.pop();
-        return prioritized_interrupt.interrupt;
-    }
-
-private:
-    _InterruptRaised _raise_interrupt(Interrupt i, u8 priority) {
-        CPUE_ASSERT(m_pending_interrupts.size() < MAX_PENDING_CAPACITY, "ICU capacity full");
-        m_pending_interrupts.push({i, priority});
-        return INTERRUPT_RAISED;
-    }
-
-    struct _PrioritizedInterrupt {
-        Interrupt interrupt;
-        u8 priority;
-    };
-    struct {
-        bool operator()(_PrioritizedInterrupt const l, _PrioritizedInterrupt const r) const { return l.priority < r.priority; }
-    } _comparator;
-    std::priority_queue<_PrioritizedInterrupt, std::vector<_PrioritizedInterrupt>, decltype(_comparator)> m_pending_interrupts{_comparator};
-};
 
 
 class CPU {
@@ -857,10 +785,7 @@ private:
 
 private:
     MMU m_mmu;
-
     PIC m_pic;
-    // NOTE: we don't support the NMI pin
-
     ICU m_icu;
     Disassembler m_disassembler;
 
---
+++ cpue/include/icu.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <mutex>
+#include <optional>
+#include <queue>
+
+#include "pic.h"
+#include "common.h"
+#include "interrupts.h"
+
+namespace CPUE {
+
+/**
+ * Interrupt Controller Unit
+ * This class encapsulates functionality related to interrupts and exceptions.
+ * It acts as a centralized location to receive interrupts from:
+ *  - the processor itself (processor-generated exceptions)
+ *  - NMI pin
+ *  - INTR pin
+ */
+class ICU {
+public:
+    friend class CPU;
+    ICU() = default;
+    ICU(ICU const&) = delete;
+
+private:
+    static constexpr u8 MAX_PENDING_CAPACITY = 127;
+    _InterruptRaised raise_interrupt(Interrupt i, u8 priority) {
+        CPUE_ASSERT(1 <= priority <= 9, "priority must be between 1 and 9");
+        return _raise_interrupt(i, priority);
+    }
+    _InterruptRaised raise_interrupt(Interrupt i) {
+        TODO_NOFAIL("Implement proper interrupt priority setting");
+        u8 priority = [&]() -> u8 {
+            switch (i.type) {
+                case InterruptType::ABORT_EXCEPTION: return 1;
+                case InterruptType::TRAP_EXCEPTION: return 2;
+                case InterruptType::FAULT_EXCEPTION: return 7;
+                case InterruptType::NON_MASKABLE_INTERRUPT: return 5;
+                case InterruptType::MASKABLE_INTERRUPT: return 6;
+                default: return 9;
+            }
+        }();
+        return raise_interrupt(i, priority);
+    }
+    _InterruptRaised nmi_raise_interrupt(Interrupt i) { fail("NMI pin is not implemented"); }
+
+    /**
+     * Instruction integral interrupts are handled as an integral part of the current instruction,
+     * so they get a virtual highest priority.
+     * NOTE: If #PF and #GP occur during fetching of an instruction, they are not integral.
+     *       If #GP occur during decoding of an instruction (cause Instruction length > 15 bytes), they are not integral.
+     *       Otherwise, they are integral.
+     * Integral Exceptions are: DE, BP, OF, BR, TS, NP, SS, GP, PF, AC, MF, XM, VE, CP,
+     */
+    _InterruptRaised raise_integral_interrupt(Interrupt i) { return _raise_interrupt(i, 0); }
+
+    std::optional<Interrupt> pop_highest_priority_interrupt() {
+        std::scoped_lock lock(m_lock);
+        if (m_pending_interrupts.empty())
+            return {};
+        auto prioritized_interrupt = m_pending_interrupts.top();
+        m_pending_interrupts.pop();
+        return prioritized_interrupt.interrupt;
+    }
+
+    _InterruptRaised _raise_interrupt(Interrupt i, u8 priority) {
+        std::scoped_lock lock(m_lock);
+        CPUE_ASSERT(m_pending_interrupts.size() < MAX_PENDING_CAPACITY, "ICU capacity full");
+        m_pending_interrupts.push({i, priority});
+        return INTERRUPT_RAISED;
+    }
+
+    struct _PrioritizedInterrupt {
+        Interrupt interrupt;
+        u8 priority;
+    };
+    struct {
+        bool operator()(_PrioritizedInterrupt const l, _PrioritizedInterrupt const r) const { return l.priority < r.priority; }
+    } _comparator;
+    std::priority_queue<_PrioritizedInterrupt, std::vector<_PrioritizedInterrupt>, decltype(_comparator)> m_pending_interrupts{_comparator};
+    std::mutex m_lock;
+};
+
+}
