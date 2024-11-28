ff0dee97932a85f532819d2a2bfa6de1f7562efc
tibotix <tizian@seehaus.net>
2024-11-28 12:56:26 +0100

    style: reformat code

--- cpue/include/checked_arithmetic.h
+++ cpue/include/checked_arithmetic.h
@@ -7,7 +7,8 @@
 namespace CPUE {
 
 template<typename R, typename... T>
-requires(std::is_same_v<R, T>&&...) && (std::is_integral_v<R> && std::is_unsigned_v<R>)constexpr R CPUE_checked_uadd(R const first, T const... factors) {
+    requires(std::is_same_v<R, T> && ...) && (std::is_integral_v<R> && std::is_unsigned_v<R>)
+constexpr R CPUE_checked_uadd(R const first, T const... factors) {
     // Consider calculation invalid if we are only adding one number
     static_assert(sizeof...(factors) > 0, "You have to add at least two numbers together.");
 
@@ -27,7 +28,8 @@ requires(std::is_same_v<R, T>&&...) && (std::is_integral_v<R> && std::is_unsigne
 }
 
 template<typename R, typename... T>
-requires(std::is_same_v<R, T>&&...) && (std::is_integral_v<R> && std::is_unsigned_v<R>)constexpr R CPUE_checked_umul(R const first, T const... factors) {
+    requires(std::is_same_v<R, T> && ...) && (std::is_integral_v<R> && std::is_unsigned_v<R>)
+constexpr R CPUE_checked_umul(R const first, T const... factors) {
     // Consider calculation invalid if we are only multiplying one number
     static_assert(sizeof...(factors) > 0, "You have to multiply at least two numbers together.");
 
